40fc2d9bea3b2907058db8f120a09565023400e1
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-01 21:42:23 +0100

    Update App.js
    added basic seed functionality for box upper face color generation

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import SceneComponent from "./SceneComponent"; // uses above component in same d
 import "./App.css";
 
 let box;
-let box2;
+let sphere;
 
 let cubeSize = 0.8;
-let widthInBlocks = 8;
+let widthInBlocks = 15;
+
+let seed = Math.random()*100000000;
 /* let sphere; */
 
 const onSceneReady = (scene) => {
   // This creates and positions a free camera (non-mesh)
   const camera = new ArcRotateCamera("camera1", -Math.PI/2, 1, 10, new Vector3(0, 0, 0), scene);
 
-  let centerCoords = (widthInBlocks*cubeSize)/2 - (cubeSize/2);
+  let centerCoords = (widthInBlocks*cubeSize)/2 - cubeSize;
   // This targets the camera to scene origin
   camera.setTarget(new Vector3(centerCoords, 0, -centerCoords));
 
@@ -25,45 +27,46 @@ const onSceneReady = (scene) => {
   camera.attachControl(canvas, true);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new HemisphericLight("light", new Vector3(0, 0.9, 0), scene);
+  const light = new HemisphericLight("light", new Vector3(0, 0.5, 0), scene);
   //const light2 = new HemisphericLight("light", new Vector3(-1, 1, -1), scene);
 
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 1;
   //light2.intensity = 0.5;
 
-  const faceColors = new Array(6);
-  faceColors[4] = new Color4(1, 0, 0, 1);
-  faceColors[1] = new Color4(0, 1, 0, 1);
-
   // Our built-in 'box' shape.
-  box = MeshBuilder.CreateBox("box", { size: cubeSize, faceColors: faceColors, }, scene);
+  /* box = MeshBuilder.CreateBox("box", { size: cubeSize, faceColors: faceColors, }, scene);
   box.edgesWidth = 1;
   box.edgesColor = new Color4(0, 0, 0, 1);
-  box.enableEdgesRendering();
+  box.enableEdgesRendering(); */
 
   let zRow = 1;
-  let xPosition = box.position.x;
+  let xPosition = 0;
   console.log(`xPosition on definition: ${xPosition}`);
-  let zPosition = box.position.z;
+  let zPosition = 0;
 
-  for (let i = 1; i < Math.pow(widthInBlocks, 2); i++) {
-    let basicBox = box.clone('basicBox' + i);
-    let boxMat = new StandardMaterial('mat' + i, scene);
-    boxMat.diffuseColor = new Color4(Math.random(), Math.random(), Math.random(), 1);
-    basicBox.material = boxMat;
-    let x = i % 8;
+  for (let i = 0; i < Math.pow(widthInBlocks, 2); i++) {
+    const faceColors = new Array(6);
+    faceColors[4] = new Color4(((seed%255) + 600)/1000, ((seed%255))/1000, ((seed%255) + 100)/1000, 1);
+    faceColors[1] = new Color4(0, 1, 0, 1);
+    box = MeshBuilder.CreateBox(`box${i}`, { size: cubeSize, faceColors: faceColors, }, scene);
+    box.edgesWidth = 1;
+    //(seed%255)/1000
+
+    box.edgesColor = new Color4(0, 0, 0, 1);
+    box.enableEdgesRendering();
+    let x = i % widthInBlocks;
     console.log(x);
     console.log(`i: ${i}`);
     xPosition = (x - (x*0.2));
-    basicBox.position.x = xPosition;
+    box.position.x = xPosition;
     if (i % widthInBlocks == 0) {
       zRow++;
       zPosition -= cubeSize;
     }
-    basicBox.position.z = zPosition;
+    box.position.z = zPosition;
   }
-  /* sphere = MeshBuilder.CreateSphere('sphere', { size: 1 }, scene); */
+  //sphere = MeshBuilder.CreateSphere('sphere', { size: 0.1 }, scene);
 
 
   box.edgesShareWithInstances = true;
